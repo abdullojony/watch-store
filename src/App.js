0033b89aebb622604f264064f129e06a261c53a7
Abdullojon Yusupov <email.abdullojon@gmial.com>
2024-12-26 19:10:18 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WatchStoreApp from './watch-store-app.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <WatchStoreApp/>
+    </div>
+  );
+}
+
+export default App;
